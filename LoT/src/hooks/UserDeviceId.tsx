9f034a597fb95e78a720dfadc0018c62525bc894
KevinKing66 <Kevin>
2025-07-27 13:37:33 -0500

    Se empieza a generar deviceId

---
+++ LoT/src/hooks/UserDeviceId.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const DEVICE_ID_KEY = 'deviceId';
+
+export const useDeviceId = (): string | null => {
+  const [deviceId, setDeviceId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedId = localStorage.getItem(DEVICE_ID_KEY);
+    if (storedId) {
+      setDeviceId(storedId);
+    } else {
+      const newId = uuidv4();
+      localStorage.setItem(DEVICE_ID_KEY, newId);
+      setDeviceId(newId);
+    }
+  }, []);
+
+  return deviceId;
+};
