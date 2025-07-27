0af8c1f295ddb41b9d2fab236c1564342e622cce
KevinKing66 <Kevin>
2025-07-27 13:41:28 -0500

    Se lleva a reducer el estado

--- LoT/src/pages/Home.tsx
+++ LoT/src/pages/Home.tsx
@@ -1,57 +1,19 @@
+import { useReducer } from "react";
 import { FuelChart } from "../component/FuelChart";
 import { SpeedChart } from "../component/SpeedChart"
 import { SpeedFuelChart } from "../component/SpeedxFuelChart";
-import type { SensorData } from '../types/sensor-data';
 import { mapToSpeedFuelChartData } from "../utils/utils";
+import { useSensorData } from "../hooks/useSensorData";
 
 export const Home = () => {
-  const data: SensorData[] =  [
-  {
-    deviceId: 'sensor-001',
-    gps: {
-      lat: 40.7128,
-      lng: -74.0060,
-      speed: 50.2,
-    },
-    temperature: 22.5,
-    fuel: {
-      current: 30,
-      capacity: 60,
-    },
-  },
-  {
-    deviceId: 'sensor-002',
-    gps: {
-      lat: 34.0522,
-      lng: -118.2437,
-      speed: 65.8,
-    },
-    temperature: 25.0,
-    fuel: {
-      current: 15,
-    },
-  },
-  {
-    deviceId: 'sensor-003',
-    gps: {
-      lat: 51.5074,
-      lng: -0.1278,
-      speed: 0,
-    },
-    temperature: 19.3,
-    fuel: {
-      current: 50,
-      capacity: 70,
-    },
-  },
-];
-const speedXFuel = mapToSpeedFuelChartData(data);
+  const [data,  ] = useReducer(useSensorData, null);
+  const speedXFuel = mapToSpeedFuelChartData(data?.sensors ?? []);
   return (
     <div>
-      <div>Home</div>
+      <div>DashBoard</div>
       <div>
-        <SpeedChart data={data}></SpeedChart>
-        <FuelChart data={data}></FuelChart>
+        <SpeedChart data={data?.sensors ?? []}></SpeedChart>
+        <FuelChart data={data?.sensors ?? []}></FuelChart>
         <SpeedFuelChart data={speedXFuel}></SpeedFuelChart>
       </div>
     </div>
---
+++ LoT/src/reducers/sensorsReducer.ts
@@ -0,0 +1,25 @@
+import type { SensorData } from "../types/sensor-data";
+
+export interface SensorsState {
+    sensors: SensorData[];
+    loading: boolean;
+    error: string | null;
+}
+
+type Action =
+    | { type: "FETCH_START" }
+    | { type: 'SET_DATA'; payload: SensorData[] }
+    | { type: "FETCH_ERROR"; payload: string };
+
+export const sensorsReducer = (state: SensorsState, action: Action): SensorsState => {
+    switch (action.type) {
+        case "FETCH_START":
+            return { ...state, loading: true, error: null };
+        case 'SET_DATA':
+            return { sensors: action.payload, loading: false, error: null };
+        case "FETCH_ERROR":
+            return { ...state, error: state.error, loading: false }
+        default:
+            return state!;
+    }
+};
