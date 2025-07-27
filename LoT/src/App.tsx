f368bc2007c1259b79daf0f6dc76819b10cb6086
KevinKing66 <Kevin>
2025-07-27 13:41:00 -0500

    se crea deviceid

--- LoT/src/App.tsx
+++ LoT/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 import { AuthProvider } from './context/AuthProvider'
+import { useDeviceId } from './hooks/UserDeviceId';
 import AppRoutes from './routes/AppRoutes'
 
 function App() {
+  useDeviceId();
   return (
     <>
       <AuthProvider>
